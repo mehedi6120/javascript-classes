dddf15fb3d270be794a7f0189b6527da34031a45
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-19 20:47:58 +0600

    class1 and class2 are added

---
+++ class1.js
@@ -0,0 +1,25 @@
+// class 1
+
+console.log(12.3+4);
+//binary
+console.log(0o5);
+//any type of decimal,binary,hexadecimal can be in console.log;
+//
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+//get only the value without decimal
+console.log(Number.parseInt('1.33'));
+//get the value with decimal
+console.log(Number.parseFloat('1.333'));
+//kono number ei infinity theke boro hote perna
+console.log(Infinity);
+//kono number ei -infinity theke choto hote perna
+console.log(-Infinity);
+//je kono kichur type bole dibe
+console.log(typeof 1233);
+console.log(typeof '1233');
+console.log(75+25);
+//console e sob mathematical operation kora jjay
+
+// class 1 end
+// class 2 start
---
+++ class2.js
@@ -0,0 +1,6 @@
+// console.log(process.argv);
+
+let x = parseInt(process.argv[2]);
+let y = parseInt(process.argv[3]);
+
+console.log(x - y);
