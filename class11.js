4e40dd19b1eebf3949a31184001984f6549c5707
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-20 20:59:45 +0600

    class11(control statement -01)

---
+++ class11.js
@@ -0,0 +1,28 @@
+// control statement -01
+if(true){
+    console.log('hello there');
+  }
+  else{
+    console.log('bye bye there');
+  }
+  //project
+  var age = 21;
+  // every comparison oparetor produce a boolean value example:
+  // console.log(age<18);
+  if(age>=18){
+    console.log('you are 18');
+  }
+  else{
+    console.log('you are not 18');
+  }
+  
+  //else if 
+  if(age>=18){
+    console.log('you are eligible');
+  }
+  else if(age>=21){
+    console.log('you are not eligible');
+  }else{
+    console.log('you got loss')
+  }
+  
