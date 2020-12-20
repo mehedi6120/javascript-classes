cc3e28798274fcf40390558f63fcf58c61f61d51
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-20 19:02:42 +0600

    class10(expression/statement/block)

---
+++ class10.js
@@ -0,0 +1,9 @@
+// expression/statement/block on javascript
+
+// {} er vitorer gulake block bole/ group hisabe bibechona kore
+{
+    //expression(it will produce a value)
+    1+2;
+    //statement(it just make an achivement)
+    console.log(console.log(3));
+  }
