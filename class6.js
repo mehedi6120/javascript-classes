8e4799f4924c7943a652e8d9b3e701779134b9d6
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-20 12:45:03 +0600

    class6

---
+++ class6.js
@@ -0,0 +1,40 @@
+// string class 2 ; properties of string
+//charAt('index number onujaiye character er position ber korbe')
+var a = "hello world";
+var b = a.charAt(0);
+//find out the char code
+var c = a.charCodeAt(0);
+//letter onujai index ber korbe
+var d = a.indexOf('o');
+//match er maddhome oi letter ta thakle seta return korbe
+var e = a.match('o');
+//replace
+var f = a.replace('o','i');
+//length ber korbe
+var g = a.length;
+// toUppercase
+var h = a.toUpperCase();
+//toLowerCase
+var i = a.toLowerCase();
+// string compare kora jay
+var j = (`hello`===`hello`);
+//trim(white space gula remove kore dibe)
+var k = '  dash dash';
+var l = k.trim().toLowerCase();
+
+ console.log(b);
+ console.log(c);
+ console.log(d);
+ console.log(e);
+ console.log(f);
+ console.log(g);
+ console.log(h);
+ console.log(i);
+ console.log(j);
+ console.log(l);
+ 
+
+
+
+
+
