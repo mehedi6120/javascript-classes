e3727e344141bc6f88af054132afbd029e8b1099
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-20 12:41:49 +0600

    class5 added

---
+++ class5.js
@@ -0,0 +1,11 @@
+//string 
+// '' and "" er vitore ja thakbe setai string
+console.log('hello world');
+var a = 'let\n s have fun';
+//string sob somoy 1 line er hobe
+`eitar vitore multi line string use kora jay
+chaile varriableo use kora jay.`
+
+var b = 55;
+// `${b}` eivabe o varriable use kora jay cottation er vitore
+// `` this is new type string
