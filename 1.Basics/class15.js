c14137ec1b792d2e6c90eb75676a44a89805758a
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-21 21:22:39 +0600

    class15(loop)

---
+++ 1.Basics/class15.js
@@ -0,0 +1,18 @@
+//while loop
+var a = 0;
+
+/*while(a<5){
+  a++; 
+  console.log(a+' boo');
+}
+console.log('stop koro')
+*/
+
+//do while loop
+do{
+  console.log(`${a} ami print`);
+  a++;
+}
+while(a<5){
+  console.log('stop');
+}
