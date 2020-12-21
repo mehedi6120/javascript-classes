1cd535db60d7545c0aa465c9ae2daec68ce41218
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-21 10:27:18 +0600

    class12(ternary operator)

---
+++ class12.js
@@ -0,0 +1,16 @@
+//ternary operator
+
+
+var age = 16 ;
+
+if (age>18){
+  console.log('shabalok');
+}
+else{
+  console.log('nabalok');
+}
+//ternary
+age>18? console.log('nah 18 na,'):console.log('nabalok chele');
+//syntex:
+//1?2:3; 
+//condition?true:false;
