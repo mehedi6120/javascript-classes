b22c7ea38cf5741d6de9d1853b5c287759debf78
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-22 13:08:15 +0600

    class16(not operator)

---
+++ 1.Basics/class16.js
@@ -0,0 +1,26 @@
+// not operator
+/*if(!true){
+  console.log('hello world')
+}
+else{
+  console.log('not')
+}*/
+var a = 'mehedi';
+if(!a){
+  console.log('a was not provided');
+}
+else{
+console.log(`hello ${a}`)
+}
+console.log(!false);
+console.log(!true);
+console.log(!"");
+console.log(!0);
+console.log(!0);
+console.log(!1);
+
+console.log(Boolean(!!!!''))
+
+
+
+
