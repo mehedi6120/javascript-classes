b6e4788f20939e13a1bff527ee922f7380691c3d
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-22 19:00:36 +0600

    class17(loop)

---
+++ 1.Basics/class17.js
@@ -0,0 +1,19 @@
+//for loop
+/*let a = 0;
+
+while(a<=10){
+  console.log(a);
+  a++;
+}*/
+// for(initial;condition;change)
+/*for(let b = 0; b<=10; b++){
+  console.log(b);
+}*/
+//find out even number
+/*for(let c = 2; c<=100; c+=2){
+  console.log(c);
+}*/
+//5 er gunitok ber kora
+for(let d = 5; d<=100 ; d+=5){
+    console.log(d);
+  }
