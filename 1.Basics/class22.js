8b15aa1d93a492159473ca1475e6cc24ffd72a77
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-25 18:53:23 +0600

    class21(function intro)

---
+++ 1.Basics/class22.js
@@ -0,0 +1,11 @@
+//function intro
+function avarage(num1,num2){
+    let answer = (num1+num2)/2;
+    return answer;
+  }
+  var c = avarage(2,4);
+  
+  console.log(c);
+  let megaavarg= avarage(avarage(5,5),avarage(5,5));
+  console.log(megaavarg);
+  
