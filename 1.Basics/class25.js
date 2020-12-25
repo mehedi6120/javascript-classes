929c344886078db8d787beb33b86abf10e40c323
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-25 20:56:50 +0600

    class25(hustings)

---
+++ 1.Basics/class25.js
@@ -0,0 +1,12 @@
+// hustings
+//javasccript age declearing gula kaj kore. 
+var subtract = function(a,b){
+    return a-b;
+  }
+  var ans = add(4,5);
+  console.log(ans);
+  console.log(subtract(4,4));
+  function add (a,b){
+    return a+b;
+  }
+  
