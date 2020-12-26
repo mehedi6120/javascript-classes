4dde7919472522686200ab370db61a99a488e5bd
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-26 15:13:30 +0600

    class26(function writing system -IIFE)

---
+++ 1.Basics/class26.js
@@ -0,0 +1,16 @@
+// iife
+// system 1 to write function
+(function(){
+    var x = 5;
+    console.log(x);
+  })()
+  // system 2 to write function
+  !function(){
+    var y = 4;
+    console.log(y)
+  }()
+  // system 3 to write function
+  +function(){
+    var y = 4;
+    console.log(y)
+  }()
