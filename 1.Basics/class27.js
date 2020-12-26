e31b3744f97a134288933c4ed53756b3353874d7
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-26 15:26:34 +0600

    class27(arrow function)

---
+++ 1.Basics/class27.js
@@ -0,0 +1,20 @@
+// arrow function
+// var function name = (arguments,arguments)=> return statement
+// jodi argument na thake tahole 
+// var function name = ()=> return statement
+var x = a=>a+1;
+
+console.log(x(2));
+console.log(x(3));
+console.log(x(6));
+//if have multiple return element
+var y = (b,c)=>{
+  console.log(b);
+  console.log(c);
+  return b-c;
+}
+console.log(y(4,5));
+
+// expression
+// ()=>return value
+// ()=>{}
