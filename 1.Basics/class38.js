64fc871c6edd93d51d370f0a873df77b0060b505
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-27 18:36:26 +0600

    class38(for in loop)

---
+++ 1.Basics/class38.js
@@ -0,0 +1,12 @@
+//for in loop
+var manush= {
+    name:"mehedi",
+    age: 21,
+    gender:"male",
+    city:"lakshmipur",
+    hometown: 'lakshmipur'
+  }
+  for (let item in manush){
+    // console.log(manush[item]);
+    console.log(`my ${item} is ${manush[item]}`)
+  }
