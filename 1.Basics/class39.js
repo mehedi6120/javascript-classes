ced572afe890d09b7775c5c7d2535fa289f19d0e
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-27 18:44:42 +0600

    class39(error handling)

---
+++ 1.Basics/class39.js
@@ -0,0 +1,10 @@
+// error handling
+// throw new Error("hello world");
+try {
+    console.log("sdfhdfdf");
+    throw new Error("cute error")
+  }catch(e){
+    console.log(e.message);
+  }
+  //eita use korle error er porer code o run korbe
+  
