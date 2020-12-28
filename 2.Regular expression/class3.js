54bcb8564a3a6296861b2c48e8652469c0685952
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-28 19:03:54 +0600

    class3(reg exp 3)

---
+++ 2.Regular expression/class3.js
@@ -0,0 +1,38 @@
+/* // regular expression 3
+hahahahahahah
+/(ha){2,}/
+
+humbaaaaaaaaaaaaaaaaaaa
+/humba{2,}?/
+
+// start and end
+
+/^ shurute thakle jeta shurute thakbe seta thaktei hobe
+/^01[35-9]\d{8}$/
+sehs e jodi $ sing thake tahole {} er por ar kono songkha nibena
+
+
+
+html tag match kora
+/<.+?>/g
+<p>hello world</p>
+
+
+color match
+/#([0-9A-F]{3}){1,2}/i
+#fff
+#FFFFFF
+#Ac8e34
+ */
+
+
+
+
+
+
+
+
+
+
+
+
