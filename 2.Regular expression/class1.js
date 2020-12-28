c864d4ffe030d105e9569d109e7e8b137848a8b0
mehedi6120 <rxmehedi531737rm@gmail.com>
2020-12-28 18:21:20 +0600

    class1(reg exp)

---
+++ 2.Regular expression/class1.js
@@ -0,0 +1,25 @@
+//Regular expression
+/* var exp = /book/i
+i = insensetive;
+g = globally;
+m = multiline //sob line e search korbe
+/book|black|back/ //pipe ta or er kaj kobe
+//groping 
+/b(oo|ac|loc)k/g
+// regular expression e (.) mane je kono kichu
+/3.\14/
+
+sir,Sir,sIR,SIR
+/sir/i
+
+thamaite parbe nah,
+thamaite parbe 
+/thamaite/mg
+
+
+
+bat,bad,boo,bug
+/b(at|ad|oo)/
+/b(a(t|d)|oo)/ */
+
+
