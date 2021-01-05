a835f5a94de9961f98ea8c8a2ae658fee2218319
mehedi6120 <rxmehedi531737rm@gmail.com>
2021-01-05 12:53:58 +0600

    class8(js browser apis)

---
+++ Dom/class8(Brower API)/index.js
@@ -0,0 +1,15 @@
+let vid = document.querySelector(".myvid");
+
+function success(stream) {
+    let vid_url = URL.createObjectURL(stream);
+    vid.src = vid_url;
+    vid.play();
+}
+function fail(err) {
+    console.log(err.message);
+}
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+})
+    .then(success, fail);
